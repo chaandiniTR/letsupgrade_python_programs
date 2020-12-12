916916fd90b1efbfa12c9ad026142864c98d463d
chaandiniTR <75902362+chaandiniTR@users.noreply.github.com>
2020-12-12 21:10:05 +0530

    Hello
    Do check and let me know, so i can correct my mistakes.

---
+++ Letsupgrade Assignment Day2.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 5 Different String Functions in Python
+
+# In[8]:
+
+
+st = "Hello LetsUpgrade Team"
+
+
+# In[3]:
+
+
+st.lower()
+
+
+# In[4]:
+
+
+st.capitalize()
+
+
+# In[5]:
+
+
+st.count("e")
+
+
+# In[6]:
+
+
+st.islower()
+
+
+# In[7]:
+
+
+st.find("Hello")
+
+
+# #  5 Different functions of the List object in Python
+# 
+
+# In[14]:
+
+
+num = [1,2,4,7]
+
+
+# In[15]:
+
+
+name= ["Chaandini","Venky","Raji"]
+
+
+# In[16]:
+
+
+lst = num + name
+
+
+# In[17]:
+
+
+lst
+
+
+# In[18]:
+
+
+lst.count("Venky")
+
+
+# In[19]:
+
+
+lst.reverse()
+
+
+# In[20]:
+
+
+lst
+
+
+# In[21]:
+
+
+lst.index("Raji")
+
+
+# In[31]:
+
+
+lst.append(5)
+
+
+# In[32]:
+
+
+lst
+
+
+# In[33]:
+
+
+lst.pop()
+
+
+# In[35]:
+
+
+lst
+
+
+# # 5 default functions of Dictionary
+# 
+# 
+
+# In[39]:
+
+
+dit= {"Nancy":"25", "Ben":"23", "Jhon":"21", "Steve":"27", "Mike":"24"}
+
+
+# In[40]:
+
+
+dit["Mike"]
+
+
+# In[41]:
+
+
+dit.get("Jhon")
+
+
+# In[45]:
+
+
+dit.keys()
+
+
+# In[46]:
+
+
+dit.values()
+
+
+# In[48]:
+
+
+dit.copy()
+
+
+# In[56]:
+
+
+dit.fromkeys("Ben")
+
+
+# In[ ]:
+
+
+
+
